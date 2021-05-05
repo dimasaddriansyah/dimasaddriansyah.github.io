d3fb9535745464568ffd80bfedbcf0d812d46800
zulfakhoerul <zulfakhoerul00@gmail.com>
2021-05-05 09:48:08 +0700

    Update

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+new Typed("#typed", {
+    strings: ["UI UX Designer", "Web Developer"],
+    typeSpeed: 30,
+    backSpeed: 20,
+    delaySpeed: 50,
+    loop: true,
+    cursorChar: "|",
+});
+
+AOS.init();
